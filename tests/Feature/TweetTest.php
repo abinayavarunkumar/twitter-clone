186d30092c40d0801884fb8ca5a97c979c3c0c04
abinayavarunkumar <abinayavarunkumar@gmail.com>
2023-10-31 16:04:28 +0530

    Create Feature test

---
+++ tests/Feature/TweetTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+
+class TweetTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+
+     public function test_can_view_home(): void
+     {
+
+        $user = User::factory()->create();
+        $response = $this
+        ->actingAs($user)
+        ->get('/home');
+
+        $response->assertOk();
+     }
+
+    public function testUserCanCreateTweet(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $response = $this->post('/tweets', ['body' => 'This is a test tweet']);
+        $this->assertDatabaseHas('tweets', ['body' => 'This is a test tweet']);
+       // $response->assertRedirect('/');
+        // Add any other necessary assertions
+    }
+}
